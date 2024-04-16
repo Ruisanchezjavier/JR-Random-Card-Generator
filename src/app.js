45b664b9613876abc95206ea0e50a9ce47c52524
Ruisanchezjavier <160360911+Ruisanchezjavier@users.noreply.github.com>
2024-04-16 01:05:27 +0000

    First steps

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let suit = ["♥", "♣", "♦", "♠"];
+  let value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Q", "K", "A"];
+
+  function generateRandomSuit() {
+    return suit[Math.floor(Math.random() * suit.length)];
+  }
+  function generateRandomValue() {
+    return value[Math.floor(Math.random() * value.length)];
+  }
+
+  let randomSuit = generateRandomSuit(suit, suit.length);
+  let randomValue = generateRandomValue(value, value.length);
+
+  let card = `${randomSuit} ${randomValue} ${randomSuit}`;
+  // console.log(card);
+
+  const htmlDocument = document.querySelector(".card");
+  htmlDocument.innerHTML = card;
+  console.log(card);
 };
