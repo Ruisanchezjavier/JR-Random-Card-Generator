147b12c3b9f830edbc6b81a87425b71291eceef6
Ruisanchezjavier <160360911+Ruisanchezjavier@users.noreply.github.com>
2024-04-21 05:47:10 +0000

    basic card generator complete

--- src/app.js
+++ src/app.js
@@ -6,8 +6,7 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-
+  // these arrays create the full deck of cards
   let suit = ["♥", "♣", "♦", "♠"];
   let value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Q", "K", "A"];
 
@@ -18,13 +17,19 @@ window.onload = function() {
     return value[Math.floor(Math.random() * value.length)];
   }
 
-  let randomSuit = generateRandomSuit(suit, suit.length);
-  let randomValue = generateRandomValue(value, value.length);
+  let randomSuit = generateRandomSuit();
+  let randomValue = generateRandomValue();
 
+  document.querySelector(".number").innerHTML = randomValue;
+  document.querySelector(".topSuit").innerHTML = randomSuit;
+  document.querySelector(".bottonSuit").innerHTML = randomSuit;
+  if (randomSuit === "♦" || randomSuit === "♥") {
+    document.querySelector(".topSuit").style.color = "red";
+    document.querySelector(".bottonSuit").style.color = "red";
+
+    // const htmlDocument = document.querySelector(".card");
+    // htmlDocument.innerHTML = card;
+  }
   let card = `${randomSuit} ${randomValue} ${randomSuit}`;
-  // console.log(card);
-
-  const htmlDocument = document.querySelector(".card");
-  htmlDocument.innerHTML = card;
   console.log(card);
 };
